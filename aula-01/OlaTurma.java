4b0591e00728a77e9656ee624016e7c7a7a26617
Cayo Nunes <cayocesard@gmail.com>
2026-02-11 00:24:21 +0000

    aula 01 - commit inicial

---
+++ aula-01/OlaTurma.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class OlaTurma {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Olá, turma! ");
+
+        System.out.println("Idade: ");
+        int idade = sc.nextInt();
+
+        System.out.println(idade);
+
+    }
+}
